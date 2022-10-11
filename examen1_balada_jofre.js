48dd9e2b16e7d0b4b0575566d39ad818ba7557bb
JofreBa <a21jofbalgon@inspedralbes.cat>
2022-10-11 12:48:43 +0200

    pregunta 3

--- examen1_balada_jofre.js
+++ examen1_balada_jofre.js
@@ -1,15 +1,45 @@
 // Node.js program to demonstrate the
-// fs.open() Method
+// fs.writeFile() method
 
-// Include the fs module
-var fs = require('fs');
+// Import the filesystem module
+const fs = require('fs');
 
-console.log("Open file!");
 
-// To open file in write and read mode,
-// create file if doesn't exists.
-fs.open('log_programa/log_file.txt', 'w+', function (err, f) {
-if (err) {
-	return console.error(err);
+let date_ob = new Date();
+
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+// current hours
+let hours = date_ob.getHours();
+
+// current minutes
+let minutes = date_ob.getMinutes();
+
+// current seconds
+let seconds = date_ob.getSeconds();
+
+// prints date in YYYY-MM-DD format
+console.log(year + "-" + month + "-" + date);
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+console.log();
+
+
+
+let data = (year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds)
+
+fs.writeFile("log_programa/log_file.txt", data, (err) => {
+if (err)
+	console.log(err);
+else {
+	console.log("File written successfully\n");
+	console.log("The written has the following contents:");
+	console.log(fs.readFileSync("log_programa/log_file.txt", "utf8"));
 }
 });
