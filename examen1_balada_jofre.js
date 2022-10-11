fb3bc967249d031d4d6182f08cf6b578dc195381
JofreBa <a21jofbalgon@inspedralbes.cat>
2022-10-11 12:15:15 +0200

    pregunta 1

--- examen1_balada_jofre.js
+++ examen1_balada_jofre.js
@@ -1,1 +1,13 @@
-//examen 1 Jofre Balada
+// Node.js program to demonstrate the
+// fs.mkdir() Method
+
+// Include fs and path module
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'log_programa'), (err) => {
+	if (err) {
+		return console.error("Directory already exists in : "+__dirname);
+	}
+	console.log('Directory log_programa created successfully in : '+__dirname);
+});
